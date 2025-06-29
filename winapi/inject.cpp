e0c21b78376d21ec4268ce5a407d5fcf311f8cb0
Ofek Lavi <flikhamud123@gmail.com>
2025-06-29 17:03:52 +0300

    fix

--- winapi/inject.cpp
+++ winapi/inject.cpp
@@ -4,7 +4,7 @@
 // path to the dll to be injected
 #define LIBRARY "C:\\Users\\User\\source\\repos\\winapi\\winapi\\my_awesome_dll.dll"
 // the PID of the injected process (notepad)
-#define PID 31852 
+#define PID 37096 
 
 int main()
 {
@@ -17,12 +17,13 @@ int main()
 	if (hProcess == NULL)
 	{
 		printf("error number %d in OpenProcess\n", GetLastError());
+		exit(1);
 	}
 
 	LPVOID dll_path = VirtualAllocEx(
 		hProcess,
 		NULL,
-		strlen(LIBRARY),
+		strlen(LIBRARY) + 1,
 		MEM_COMMIT | MEM_RESERVE,
 		PAGE_READWRITE
 
@@ -31,38 +32,31 @@ int main()
 	if (dll_path == NULL)
 	{
 		printf("error number %d in VirtualAllocEx\n", GetLastError());
+		CloseHandle(hProcess);
+		exit(1);
 	}
 
 	BOOL write = WriteProcessMemory(
 		hProcess,
 		dll_path,
 		LIBRARY,
-		strlen(LIBRARY),
+		strlen(LIBRARY) + 1,
 		NULL
 	);
 	if (not write)
 	{
 		printf("error number %d in WriteProcessMemory\n", GetLastError());
+		CloseHandle(hProcess);
+		VirtualFree(dll_path, strlen(LIBRARY) + 1, MEM_DECOMMIT);
+		VirtualFree(dll_path, 0, MEM_RELEASE);
+		exit(1);
 	}
 
 
-	HMODULE LoadLibraryhModule = LoadLibraryA("Kernel32.dll");
+	
+	LPTHREAD_START_ROUTINE LoadLibraryAddress = (LPTHREAD_START_ROUTINE)&LoadLibraryA;
 
-	if (LoadLibraryhModule == NULL)
-	{
-		printf("error number %d in LoadLibraryA\n", GetLastError());
-	}
-
-	LPTHREAD_START_ROUTINE LoadLibraryAddress = (LPTHREAD_START_ROUTINE)GetProcAddress(
-		LoadLibraryhModule,
-		"LoadLibraryA"
-	);
-
-	if (LoadLibraryAddress == NULL)
-	{
-		printf("error number %d in GetProcAddress\n", GetLastError());
-	}
-
+	
 	DWORD threadId;
 	HANDLE hThread = CreateRemoteThread(
 		hProcess,
@@ -77,12 +71,21 @@ int main()
 	if (hThread == NULL)
 	{
 		printf("error number %d in CreateRemoteThread\n", GetLastError());
+		CloseHandle(hProcess);
+		VirtualFree(dll_path, strlen(LIBRARY) + 1, MEM_DECOMMIT);
+		VirtualFree(dll_path, 0, MEM_RELEASE);
+		exit(1);
 	}
 
 	WaitForSingleObject(
 		hThread,
 		INFINITE
 	);
+
+	CloseHandle(hProcess);
+	VirtualFree(dll_path, strlen(LIBRARY) + 1, MEM_DECOMMIT);
+	VirtualFree(dll_path, 0, MEM_RELEASE);
+	CloseHandle(hThread);
 
 	return 0;
 }

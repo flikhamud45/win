df401fd122763fb17f5d477263c9c336d08684c9
Ofek Lavi <flikhamud123@gmail.com>
2025-06-29 16:44:20 +0300

    solve

--- winapi/inject.cpp
+++ winapi/inject.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 // path to the dll to be injected
-#define LIBRARY "C:\\Users\\User\\source\\repos\\winapi\\winapi\\my_awesome_dll.dll"
+#define LIBRARY "C:\\Users\\User\\source\\repos\\winapi\\winapi\\mydll.dll"
 // the PID of the injected process (notepad)
 #define PID 37096 
 
